2fa82b579f5d9fbd1dd8fa039cacfa07696e985a
Gonçalo Morais <gnclmorais@gmail.com>
2015-06-22 18:39:12 +0100

    A single donwload works. Not bad.
    However, it doesn’t work as it should yet.

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 var request = require('request-promise')
 var cheerio = require('cheerio')
 var Promise = require('bluebird')
+//var fs = require('fs')
+var fs = Promise.promisifyAll(require('fs'))
 
 var webpage = 'http://musicforprogramming.net'
 
 request(webpage)
   .then(getTrackPages)
   .then(getTrackLinks)
+  .then(downloadTracks)
+  .then(function () {
+    console.log('All done!')
+  })
   .then(console.log)
   .catch(console.error)
 
@@ -38,10 +44,23 @@ function extractTrackLink(html) {
   return track.attr('href')
 }
 
+function downloadTracks(tracks) {
+  return Promise.reduce(tracks, function (total, track) {
+    return downloadTrack(track).then(function () {
+      return total += 1;
+    })
+  }, 0)
+}
+
 function downloadTrack(track) {
   // Try to get a proper name, fallback if fails
   var name = track.match(/\d+-[a-z_]+\.mp3/g)
   name = name ? name[0] : track.split('-')[1]
 
-  return request(track).pipe(fs.createWriteStream(name))
+  //console.log(track)
+  return new Promise(function () {
+    return request(track).pipe(fs.createWriteStream(name)).on('end', function () {
+      return 'Finish!'
+    })
+  })
 }
